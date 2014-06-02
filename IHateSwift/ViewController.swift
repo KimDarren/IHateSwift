3c5fb70ab8a0c94073f721ca1903ec8230b44468
KimDarren <dev.darrenkim@gmail.com>
2014-06-03 08:48:24 +0900

    Initial Commit

---
+++ IHateSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IHateSwift
+//
+//  Created by Klavis Mac Mini on 2014. 6. 3..
+//  Copyright (c) 2014년 DarrenLine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
